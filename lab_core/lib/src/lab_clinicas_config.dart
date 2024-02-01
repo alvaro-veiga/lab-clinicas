0589ce0f54cb8d5c9b1e4e37f1f4e92cea472e12
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-01-31 23:19:11 -0300

    create sys global configuration

---
+++ lab_core/lib/src/lab_clinicas_config.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LabClinicasCoreConfig extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text('Home'),
+    );
+  }
+}

e83104778edd3931a18b89f0ef7be299f8727e32
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-01-31 22:10:39 -0300

    add interceptor for token request with dio

---
+++ lab_core/lib/src/interceptors/auth_interceptors.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+//interceptador para autenticação de token com dio
+final class AuthInteceptor extends Interceptor {
+
+  //sobrescreve o método onRequest para adicionar o token de autenticação
+  @override
+  Future <void> onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    
+    //desestrutra as opções de o interceptador do dio para extrair os headers e o extra
+    final RequestOptions(:headers, :extra) = options;
+    
+    //chave do header de autenticação
+    const authHeaderKey = 'Authorization';
+    
+    //remove o header de autenticação ja existente
+    headers.remove(authHeaderKey);
+
+    //adiciona o header de autenticação se o extra for verdadeiro
+    if (extra case {'DIO_AUTH_KEY': true}) {
+      final sp = await SharedPreferences.getInstance();
+      headers.addAll({
+        authHeaderKey: '${sp.getString('cwsMXDtuP447WZQ63nM4dWZ3RppyMl')}'
+      });
+    }
+    //chama o método onRequest do super para continuar a requisição
+    super.onRequest(options, handler);
+  }
+}
